1924ec1448cde844d90dbafb88c66da0a90d8b9e
woody8903 <164841391+woody8903@users.noreply.github.com>
2024-03-27 11:39:38 +0900

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+void swap(int x, int y);
+
 int main(void)
 {
-    int a = 10;
-    int *p = &a;
-    double *pd;
-    
-    pd = p;
-    printf("%lf\n", *pd);
-    
+    int a = 10, b = 20;
+
+    swap(a, b);
+    printf("a:%d, b:%d\n", a, b);
+
     return 0;
+}
+
+void swap(int x, int y)
+{
+    int temp;
+
+    temp = x;
+    x = y;
+    y = temp;
 }
