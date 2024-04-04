ee568b027be764a54723b31367564aa7f9662743
woody8903 <164841391+woody8903@users.noreply.github.com>
2024-04-04 16:01:33 +0900

    Add files via upload

--- 4. 04 python2.py
+++ 4. 04 python2.py
@@ -1,20 +1,29 @@
 from tkinter import*
+import xlsxwriter
+window = Tk()
 
 window = Tk()
-photo = PhotoImage(file = 'C:\Users\djkang\Desktop\noname.jpg')
+photo = PhotoImage(file = 'C:/Users/djkang/Desktop/noname.gif')
 h = photo.height()
 w = photo.width()
 print('이미지 크기:',h, 'x', w)
 
+workbook = xlsxwriter.Workbook('C:/Users/djkang/Desktop/noname.gif')
 photoR = [[0 for _ in range(h)] for _ in range(w)]
 photoG = [[0 for _ in range(h)] for _ in range(w)]
 photoB = [[0 for _ in range(h)] for _ in range(w)]
 
+workbook = xlsxwriter.Workbook('C:/Users/djkang/Desktop/noname.gif')
+worksheetR = workbook.add_worksheet('photoR')
+worksheetG = workbook.add_worksheet('photoG')
+worksheetB = workbook.add_worksheet('photoB')
+
 for i in range(w):
     for k in range(h):
         r, g, b = photo.get(i,k)
-        photoR[i][k] = r
-        photoG[i][k] = g
-        photoB[i][k] = b
+        worksheetR.write(i, k, photoR[i][k])
+        worksheetG.write(i, k, photoG[i][k])
+        worksheetB.write(i, k, photoB[i][k])
 
-print('[50][50] 위치의 RGB 값: ', photoR[50][50], photoG[50][50], photoB[50][50])
+    workbook.close()
+    print('Save. Ok~')
